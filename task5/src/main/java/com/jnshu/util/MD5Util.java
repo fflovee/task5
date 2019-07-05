e07dc17496e636b07888367c56628ceb423ffe90
fflovee <592005470@qq.com>
2019-07-06 01:51:17 +0800

    task5-1.2

--- task5/src/main/java/com/jnshu/util/MD5Util.java
+++ task5/src/main/java/com/jnshu/util/MD5Util.java
@@ -3,6 +3,7 @@ package com.jnshu.util;
 import org.apache.commons.codec.binary.Hex;
 
 import java.security.MessageDigest;
+import java.util.Random;
 
 /**
  * @ClassName MD5Util
@@ -23,7 +24,7 @@ public class MD5Util {
             // org.apache.commons.codec包的Hex.encodeHexString（Byte[] bytes）可将byte[]转为16进制字符串
             // int a=1/0;
             // String ps = new String(new Hex().encode(digest));
-            System.out.println("========================");
+            System.out.println("============生成MD5============");
             return new String(new Hex().encode(digest));
             // return ps;
 
@@ -34,9 +35,41 @@ public class MD5Util {
         return "------------------生成MD5错误--------------------";
     }
 
+    public static String getSaltMD5(String password) {
+        //生成一个16位的随机数:
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder(16);
+        stringBuilder.append(random.nextInt(99999999)).append(99999999);
+        int len = stringBuilder.length();
+        if (len < 16) {
+            for (int i = 0; i < 16 - len; i++) {
+                stringBuilder.append("0");
+            }
+        }
+        String salt = stringBuilder.toString();
+        password = md5Hex(password + salt);
+        char[] cs = new char[48];
+        for (int i = 0; i < 48; i += 3) {
+            cs[i] = password.charAt(i / 3 * 2);
+            char c = salt.charAt(i / 3);
+            cs[i + 1] = c;
+            cs[i + 2] = password.charAt(i / 3 * 2 + 1);
+        }
+
+        return String.valueOf(cs);
+    }
+
     // 验证password通过MD5加密后和数据库中存的密码是否一致
-    public static boolean verPassword(String password,String md5password) {
-        return md5password.equals(md5Hex(password));
+    public static boolean verPassword(String password, String md5password) {
+        char[] cs1 = new char[32];
+        char[] cs2 = new char[16];
+        for (int i = 0; i < 48; i += 3) {
+            cs1[i / 3 * 2] = md5password.charAt(i);
+            cs2[i / 3] = md5password.charAt(i + 1);
+            cs1[i / 3 * 2 + 1] = md5password.charAt(i + 2);
+        }
+        String salt = new String(cs2);
+        return md5Hex(password + salt).equals(String.valueOf(cs1));
     }
 
 
